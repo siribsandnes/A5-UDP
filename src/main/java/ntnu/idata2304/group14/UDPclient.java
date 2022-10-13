ec30d46be8da6f9d75d5ec0c55c16804c2ad747b
sirisandnes <siribsa@stud.ntnu.no>
2022-10-13 10:09:06 +0200

    Fixed logic for wordcount

--- src/main/java/ntnu/idata2304/group14/UDPclient.java
+++ src/main/java/ntnu/idata2304/group14/UDPclient.java
@@ -6,37 +6,21 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class UDPclient {
+
     public void run(String initMessage, String serverAdress, int serverPort){
         try {
-            // Sends message to server
-            byte[] initMessageBytes = initMessage.getBytes();
             DatagramSocket clientSocket = new DatagramSocket();
-            InetAddress serverAddress = InetAddress.getByName(serverAdress);
-            DatagramPacket sendPacket = new DatagramPacket(initMessageBytes, initMessageBytes.length, serverAddress,
-                    serverPort);
-            clientSocket.send(sendPacket);
 
-            //waits for response
-            byte[] responseDataBuffer = new byte[1024]; // Reserve a bit more space than one would normally need
-            DatagramPacket receivePacket = new DatagramPacket(responseDataBuffer, responseDataBuffer.length);
-            clientSocket.receive(receivePacket);
-            String modifiedSentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            sendToServer(clientSocket, initMessage, serverAdress, serverPort); // Sends message "task" to server
+            String sentenceFromServer = getFromServer(clientSocket); // Gets sentence from server
+            String processedSentence = processServerResponse(sentenceFromServer); //Process the sentence from server
+            sendToServer(clientSocket, processedSentence, serverAdress, serverPort); //Sends sentence type and amount of words to server
+            String responseFromServer = getFromServer(clientSocket); //Gets response from server, either ok or error
 
-            System.out.println("Response from server: " + modifiedSentence);
-            System.out.println( processServerResponse(modifiedSentence));
 
-            //
-            byte[] answerToSensor = processServerResponse(modifiedSentence).getBytes();
-            DatagramPacket sendPacket2 = new DatagramPacket(answerToSensor, answerToSensor.length, serverAddress,serverPort);
-            clientSocket.send(sendPacket2);
-
-            byte[] responseDataBuffer2 = new byte[1024];
-            DatagramPacket receivePacket2 = new DatagramPacket(responseDataBuffer2,responseDataBuffer2.length);
-            clientSocket.receive(receivePacket2);
-            String modifiedReply = new String(receivePacket2.getData(), 0, receivePacket2.getLength());
-
-            System.out.println("Second response from server: " + modifiedReply);
-
+            System.out.println("Sentence from server: " + sentenceFromServer);
+            System.out.println("Sentence type and length: " + processedSentence);
+            System.out.println("Response from server: " + responseFromServer);
 
 
             clientSocket.close();
@@ -44,6 +28,32 @@ public class UDPclient {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void sendToServer(DatagramSocket socket, String message, String serverAdress, int serverPort ){
+
+       try {
+           byte[] messageInBytes = message.getBytes();
+           InetAddress serverAddress = InetAddress.getByName(serverAdress);
+           DatagramPacket sendPacket = new DatagramPacket(messageInBytes, messageInBytes.length, serverAddress,
+                   serverPort);
+           socket.send(sendPacket);
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+    }
+
+    private String getFromServer(DatagramSocket socket){
+       try {
+           byte[] responseDataBuffer = new byte[1024];
+           DatagramPacket receivePacket = new DatagramPacket(responseDataBuffer, responseDataBuffer.length);
+           socket.receive(receivePacket);
+           String modifiedSentence = new String(receivePacket.getData(), 0, receivePacket.getLength());
+           return modifiedSentence;
+
+       }catch (IOException e){
+           throw new RuntimeException(e);
+       }
     }
 
 
@@ -59,7 +69,12 @@ public class UDPclient {
 
     private String checkWordCount(String sentence){
         String [] words = sentence.split(" ");
-        String wordCount = Integer.toString(words.length);
+        int amountOfWords = words.length;
+
+        if(amountOfWords == 1){
+            amountOfWords = 0;
+        }
+        String wordCount = Integer.toString(amountOfWords);
         return wordCount;
     }
 
